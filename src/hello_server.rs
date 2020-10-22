a058c8cb7792e289024665d2b926005a3110fe6a
Kaciras <Kaciras@outlook.com>
2020-10-22 22:59:35 +0800

    add resources folder

--- src/hello_server.rs
+++ src/hello_server.rs
@@ -5,8 +5,11 @@ use std::io::{Read, Write};
 use std::{fs, thread};
 
 fn main() {
-	let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+	let address = "127.0.0.1:7878";
+	let listener = TcpListener::bind(address).unwrap();
 	let pool = thread_pool::ThreadPool::new(4);
+
+	println!("Server is listening on http://{}", address);
 
 	for stream in listener.incoming() {
 		let stream = stream.unwrap();
@@ -21,9 +24,9 @@ fn handle_connection(mut stream: TcpStream) {
 	let get = b"GET / HTTP/1.1\r\n";
 
 	let (status, filename) = if buffer.starts_with(get) {
-		(200, "src/hello.html")
+		(200, "resources/hello.html")
 	} else {
-		(404, "src/hello.html") // 我懒得再搞一个404文件
+		(404, "resources/404.html")
 	};
 
 	stream.write(format!("HTTP/1.1 {} OK\r\n\r\n", status).as_bytes()).unwrap();
