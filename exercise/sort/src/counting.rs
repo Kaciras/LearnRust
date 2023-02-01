ef4951576de67f6c572083bc29ff85a6918dfccc
Kaciras <Kaciras@protonmail.com>
2023-02-01 10:58:11 +0800

    implement counting sort

---
+++ exercise/sort/src/counting.rs
@@ -0,0 +1,15 @@
+/// 计数排序，O(n) 级别的算法，对数据有要求，必须能够映射到有序的数字。
+/// 原理就是开一个与值域同样大小的数组，统计每个元素出现的次数，完了根据次数再添加元素即可。
+/// 该算法拥有极快的速度，但需要消耗数据值域大小的数组空间。
+pub fn sort(array: &mut [u32], max: u32) {
+	let mut counts = vec![0; max as usize];
+	for v in array.as_ref() {
+		counts[*v as usize] += 1;
+	}
+	let mut i = 0;
+	for k in 0..counts.len() {
+		let s = i;
+		i += counts[k];
+		array[s..i].fill(k as u32);
+	}
+}
--- exercise/sort/src/lib.rs
+++ exercise/sort/src/lib.rs
@@ -6,7 +6,15 @@ pub mod bubble;
 pub mod merge;
 pub mod selection;
 
+pub mod counting;
+
 type SortFn = fn(&mut [u32]) -> ();
+
+const VALUE_RANGE: u32 = 1000;
+
+fn bound_counting(array: &mut [u32]) {
+	counting::sort(array, VALUE_RANGE)
+}
 
 #[cfg(test)]
 mod tests {
@@ -17,15 +25,18 @@ mod tests {
 	#[fixture]
 	fn rand_nums(#[default(10)] length: usize) -> Vec<u32> {
 		return rand::thread_rng()
-			.sample_iter(Uniform::from(0..1000))
+			.sample_iter(Uniform::from(0..VALUE_RANGE))
 			.take(length).collect();
 	}
 
 	#[template]
 	#[rstest]
-	#[case::selection(selection::sort)]
-	#[case::merge(merge::sort)]
-	#[case::bubble(bubble::sort)]
+	#[case::counting(bound_counting)]
+
+	// #[case::insertion(insertion::sort)]
+	// #[case::selection(selection::sort)]
+	// #[case::merge(merge::sort)]
+	// #[case::bubble(bubble::sort)]
 	fn algorithms(#[case] algorithm: SortFn) {}
 
 	#[apply(algorithms)]
@@ -37,30 +48,30 @@ mod tests {
 
 	#[apply(algorithms)]
 	fn single(#[case] algorithm: SortFn) {
-		let mut array = [8964];
+		let mut array = [896];
 		algorithm(&mut array);
-		assert_eq!(array, [8964]);
+		assert_eq!(array, [896]);
 	}
 
 	#[apply(algorithms)]
-	fn odd(#[case] algorithm: SortFn, #[with(10)] mut rand_nums: Vec<u32>) {
-		let mut expected = rand_nums.clone();
+	fn odd(#[case] algorithm: SortFn, #[with(10)] mut nums: Vec<u32>) {
+		let mut expected = nums.clone();
 
-		algorithm(rand_nums.as_mut_slice());
-		println!("{:?}", rand_nums);
+		algorithm(nums.as_mut_slice());
+		// println!("{:?}", nums);
 
 		expected.sort();
-		assert_eq!(expected, rand_nums);
+		assert_eq!(expected, nums);
 	}
 
 	#[apply(algorithms)]
-	fn even(#[case] algorithm: SortFn, #[with(11)] mut rand_nums: Vec<u32>) {
-		let mut expected = rand_nums.clone();
+	fn even(#[case] algorithm: SortFn, #[with(11)] mut nums: Vec<u32>) {
+		let mut expected = nums.clone();
 
-		algorithm(rand_nums.as_mut_slice());
+		algorithm(nums.as_mut_slice());
 		// println!("{:?}", array);
 
 		expected.sort();
-		assert_eq!(expected, rand_nums);
+		assert_eq!(expected, nums);
 	}
 }
